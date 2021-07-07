7b333ba150d824557926b77d25f2d3b9141106ff
Daniil <medvedevdaniila@gmail.com>
2021-07-07 18:50:53 +0300

    day2

--- masutils.cpp
+++ masutils.cpp
@@ -19,8 +19,17 @@ void initByZeroMas(int* imas, int size)  {
 
 // Вариант 1:
 // Написать функцию инициализации size элементов массива заданным значением val.
-void initMas(int* imas, int size, int val)  {
+#include <iostream>
+using namespace std;
+void test(int* imas, int size)
+{
+initMas(imas,size,1);
+for(int i = 0; i < size; i++)
+if(imas[i] != 1) cout « "error";
 
+initMas(imas,size,-13);
+for(int i = 0; i < size; i++)
+if(imas[i] != -13) cout « "error";
 }
 
 //Вариант 2:
@@ -104,8 +113,31 @@ int testInitByZeroMas() {
 // Вариант 1:
 // Написать функцию инициализации size элементов массива заданным значением val.
 
-int testInitMas()  {
-  return -1;
+#include <iostream>
+using namespace std;
+void initMas(int* imas, int size, int val)
+{
+    for (int i = 0; i < size; i++)
+        imas[i] = val;
+}
+
+
+void test(int* imas, int size)
+{
+    initMas(imas, size, 1);
+    for (int i = 0; i < size; i++)
+        if (imas[i] != 1)  cout << "error";
+
+    initMas(imas, size, -13);
+    for (int i = 0; i < size; i++)
+        if (imas[i] != -13) cout << "error";
+}
+int main()
+{
+    int imas[10];
+    int size = 6;
+    int val = 5;
+    initMas(imas, size, val)
 }
 
 //Вариант 2:
